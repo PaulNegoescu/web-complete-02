6c5ae27452ce402872089a49bb016cfa0ee104ee
Paul Negoescu <paul.negoescu@nagarro.com>
2025-07-08 20:46:33 +0300

    React code up to css modules

---
+++ src/features/Counter/Counter.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
+import styles from './Counter.module.css';
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  // let cls = '';
+  // if(count > 0) {
+  //   cls = 'positive';
+  // } else if(count < 0) {
+  //   cls = 'negative';
+  // }
+
+  // function handleClick(e) {
+  //   const type = e.target.dataset.type;
+  //   if (type === 'increment') {
+  //     setCount(count + 1);
+  //   } else {
+  //     setCount(count - 1);
+  //   }
+  // }
+
+  function handleClick(diff = 1) {
+    setCount(count + diff);
+  }
+
+  return (
+    <>
+      <h1 title={`My count is ${count}!`}>Counter</h1>
+      {/* <output className={count > 0 ? 'positive' : count < 0 ? 'negative' : ''}>{count}</output> */}
+      <output className={clsx({ [styles.positive]: count > 0, [styles.negative]: count < 0 })}>
+        {count}
+      </output>
+      <div>
+        <button data-type="decrement" onClick={() => handleClick(-1)}>
+          -
+        </button>
+        <button data-type="increment" onClick={() => handleClick(1)}>
+          +
+        </button>
+      </div>
+    </>
+  );
+}
